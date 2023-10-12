3b803e4169d606a5202aa5ff8be7d74526df3979
Aleksei <alehazaitsek@gmail.com>
2023-10-12 12:04:37 +0300

    ensimmäinen kommiti

---
+++ src/Laskuri.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import React, {useState} from 'react'
+
+const Laskuri = (props) => {
+
+//Komponentin tilan määritys
+const [luku, setLuku] = useState(0)
+
+const nollaaLaskuri = () => {
+    setLuku(0)
+}
+  return (
+    <>
+    <h3>{luku}</h3>
+
+    <button onClick={() => setLuku(luku + 1) }>+</button>
+    <button onClick={() => setLuku(luku - 1) }>-</button>     
+    <button onClick={nollaaLaskuri}>0</button> 
+
+
+    <button onClick={props.huomio}>huomio</button>
+
+    </>
+  )
+}
+
+export default Laskuri
